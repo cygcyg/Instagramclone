9c452000af016e58cf8f0e414ae4f38fb972f285
cygcyg <satoshi_ohkawa@oizu.co.jp>
2018-07-27 02:00:03 +0900

    課題取組み前

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 大川哲史 on 2018/07/27.
+//  Copyright © 2018年 cygcyg. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
